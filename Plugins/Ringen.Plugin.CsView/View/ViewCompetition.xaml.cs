c4176f617fae2caf111e5d695ecd67958a0bb808
Christian Hennerfeind <christian.hennerfeind@bmw.de>
2020-06-25 12:15:52 +0200

    Anzeige Kampfliste optimiert

--- Plugins/Ringen.Plugin.CsView/View/ViewCompetition.xaml.cs
+++ Plugins/Ringen.Plugin.CsView/View/ViewCompetition.xaml.cs
@@ -45,5 +45,37 @@ namespace Ringen.Plugin.CsView
         {
             Competition = Explorer.SelectedItem as Core.CS.Competition;
         }
+
+        /// <summary>
+        /// Handles property changed event for the ItemsPerRow property, constructing
+        /// the required ItemsPerRow elements on the grid which this property is attached to.
+        /// </summary>
+        private static void OnItemsPerRowPropertyChanged(DependencyObject d,
+                            DependencyPropertyChangedEventArgs e)
+        {
+            Grid grid = d as Grid;
+            int itemsPerRow = (int)e.NewValue;
+
+            // construct the required row definitions
+            grid.LayoutUpdated += (s, e2) =>
+            {
+                var childCount = grid.Children.Count;
+
+                // add the required number of row definitions
+                int rowsToAdd = (childCount - grid.RowDefinitions.Count) / itemsPerRow;
+                for (int row = 0; row < rowsToAdd; row++)
+                {
+                    grid.RowDefinitions.Add(new RowDefinition());
+                }
+
+                // set the row property for each chid
+                for (int i = 0; i < childCount; i++)
+                {
+                    var child = grid.Children[i] as FrameworkElement;
+                    Grid.SetRow(child, i / itemsPerRow);
+                }
+            };
+        }
+
     }
 }
