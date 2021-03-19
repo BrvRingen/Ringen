4fa48401e7bc9e6aea1090bb2d90ff1205d14ed0
Badewitz, Alexander <a.badewitz@adb-solutions.de>
2021-03-19 19:31:23 +0100

    add cache

---
+++ src/Ringen.Schnittstelle.Caching/Services/ApiCache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using MonkeyCache.FileStore;
+using Plugin.Connectivity;
+
+namespace Ringen.Schnittstelle.Caching.Services
+{
+    public class ApiCache
+    {
+        public ApiCache()
+        {
+            Barrel.ApplicationId = "RingenApiCache";
+        }
+
+        public async Task<T> Get_und_Cache_Daten<T>(string key, Func<Task<T>> getDatenMethode, TimeSpan cacheAblaufIn)
+        {
+            //Dev handle online/offline scenario
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return Barrel.Current.Get<T>(key: key);
+            }
+
+            //Dev handles checking if cache is expired
+            if (!Barrel.Current.IsExpired(key: key))
+            {
+                return Barrel.Current.Get<T>(key: key);
+            }
+
+            T apiDaten = await getDatenMethode();
+
+            //Saves the cache and pass it a timespan for expiration
+            Barrel.Current.Add(key: key, data: apiDaten, expireIn: cacheAblaufIn);
+
+            return apiDaten;
+        }
+    }
+}
--- src/Ringen.Schnittstelle.Caching/Services/ApiSaisonInformationenMitCache.cs
+++ src/Ringen.Schnittstelle.Caching/Services/ApiSaisonInformationenMitCache.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using MonkeyCache.FileStore;
 using Ringen.Schnittstellen.Contracts.Models;
 using Ringen.Schnittstellen.Contracts.Services;
 
@@ -11,40 +13,62 @@ namespace Ringen.Schnittstelle.Caching.Services
     public class ApiSaisonInformationenMitCache : IApiSaisonInformationen
     {
         private IApiSaisonInformationen _api;
+        private readonly ApiCache _apiCache;
+        private TimeSpan _classCacheTime;
 
         public ApiSaisonInformationenMitCache(IApiSaisonInformationen api)
         {
             _api = api;
+            _apiCache = new ApiCache();
+            _classCacheTime=TimeSpan.FromSeconds(1);
         }
 
-        public Task<List<Liga>> GetLigenAsync(string saisonId)
+        public async Task<List<Liga>> GetLigenAsync(string saisonId)
         {
-            return _api.GetLigenAsync(saisonId);
+            MethodBase m = MethodBase.GetCurrentMethod();
+            var cacheKey = m.ReflectedType.FullName;
+
+            return await _apiCache.Get_und_Cache_Daten(cacheKey, async () => { return await _api.GetLigenAsync(saisonId); }, _classCacheTime);
         }
 
-        public Task<Tuple<Saison, List<Leistungsklasse>>> GetSaisonAsync(string saisonId)
+        public async Task<Tuple<Saison, List<Leistungsklasse>>> GetSaisonAsync(string saisonId)
         {
-            return _api.GetSaisonAsync(saisonId);
+            MethodBase m = MethodBase.GetCurrentMethod();
+            var cacheKey = m.ReflectedType.FullName;
+
+            return await _apiCache.Get_und_Cache_Daten(cacheKey, async () => { return await _api.GetSaisonAsync(saisonId); }, _classCacheTime);
         }
 
-        public Task<List<Saison>> GetSaisonsAsync()
+        public async Task<List<Saison>> GetSaisonsAsync()
         {
-            return _api.GetSaisonsAsync();
+            MethodBase m = MethodBase.GetCurrentMethod();
+            var cacheKey = m.ReflectedType.FullName;
+
+            return await _apiCache.Get_und_Cache_Daten(cacheKey, async () => { return await _api.GetSaisonsAsync(); }, _classCacheTime);
         }
 
-        public Task<List<Mannschaft>> GetMannschaftenAsync(string saisonId, string ligaId, string tableId)
+        public async Task<List<Mannschaft>> GetMannschaftenAsync(string saisonId, string ligaId, string tableId)
         {
-            return _api.GetMannschaftenAsync(saisonId, ligaId, tableId);
+            MethodBase m = MethodBase.GetCurrentMethod();
+            var cacheKey = m.ReflectedType.FullName;
+
+            return await _apiCache.Get_und_Cache_Daten(cacheKey, async () => { return await _api.GetMannschaftenAsync(saisonId, ligaId, tableId); }, _classCacheTime);
         }
 
-        public Task<List<EinzelkampfSchema>> GetMannschaftskampfSchemaAsync(string saisonId, string wettkampfId)
+        public async Task<List<EinzelkampfSchema>> GetMannschaftskampfSchemaAsync(string saisonId, string wettkampfId)
         {
-            return _api.GetMannschaftskampfSchemaAsync(saisonId, wettkampfId);
+            MethodBase m = MethodBase.GetCurrentMethod();
+            var cacheKey = m.ReflectedType.FullName;
+
+            return await _apiCache.Get_und_Cache_Daten(cacheKey, async () => { return await _api.GetMannschaftskampfSchemaAsync(saisonId, wettkampfId); }, _classCacheTime);
         }
 
-        public Task<List<Kampftag>> GetKampftageAsync(string saisonId)
+        public async Task<List<Kampftag>> GetKampftageAsync(string saisonId)
         {
-            return _api.GetKampftageAsync(saisonId);
+            MethodBase m = MethodBase.GetCurrentMethod();
+            var cacheKey = m.ReflectedType.FullName;
+
+            return await _apiCache.Get_und_Cache_Daten(cacheKey, async () => { return await _api.GetKampftageAsync(saisonId); }, _classCacheTime);
         }
     }
 }
